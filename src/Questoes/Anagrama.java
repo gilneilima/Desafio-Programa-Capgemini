00c39e1b9bf5a1bf7c160e6927ec06cce0b251f7
Gilnei Lima <gilneilima.rj@gmail.com>
2022-02-21 03:26:24 -0300

    Término questão 2.

---
+++ src/Questoes/Anagrama.java
@@ -0,0 +1,10 @@
+package Questoes;
+
+public class Anagrama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Questoes/SenhaForte.java
+++ src/Questoes/SenhaForte.java
@@ -1,9 +1,57 @@
 package Questoes;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SenhaForte {
 
+	private static final int tamanhoSenhaMinima = 6;
+	private static final int tamanhoSenhaMaxima = 8;
+
+	private static final String MODELO_SENHA = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()-+]).{"
+			+ tamanhoSenhaMinima + "," + tamanhoSenhaMaxima + "}$";
+
+	private static final Pattern pattern = Pattern.compile(MODELO_SENHA);
+
+	public static boolean validarSenha(final String senha) {
+		Matcher matcher = pattern.matcher(senha);
+		if (matcher.matches() == true) {
+			System.out.println("Senha validada com Sucesso!");
+		}
+		return matcher.matches();
+	}
+
+	public static String validarTamanho(String senha) {
+		if (senha.length() < tamanhoSenhaMinima) {
+			int digitos = ((tamanhoSenhaMinima) - (senha.length()));
+			System.out.println("Digitos que falta para formar senha mínima: " + digitos);
+		}
+		return senha;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		try {
+			boolean senhaValidada;
+			do {
+				System.out.println("Sua senha deve possuir: \r\n" + "No mínimo 6 caracteres.\r\n"
+						+ "No mínimo 1 digito.\r\n" + "No mínimo 1 letra em minúsculo.\r\n"
+						+ "No mínimo 1 letra em maiúsculo.\r\n" + "No mínimo 1 caractere especial: !@#$%^&*()-+\r\n");
+				System.out.println("Digite sua senha: ");
+				String senha = input.nextLine();
+				senhaValidada = validarSenha(validarTamanho(senha));
+				if (senhaValidada == false) {
+					System.err.println("Senha não validada!");
+				}
+			} while (!senhaValidada == true);
+		} catch (Exception e) {
+			System.err.println("Senha Inválida");
+		} finally {
+			input.close();
+		}
 
 	}
 
